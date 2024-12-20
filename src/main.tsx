f9d425ef6e1bf578f0767fa122848d14a49f0196
Peem Phurit <phurit.dua.workspace@gmail.com>
2024-12-21 01:57:17 +0700

    Feat: Clean Code

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,17 @@ import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(
-            registration => {
-                console.log('SW registered: ', registration);
-            },
-            error => {
-                console.error('SW registration failed: ', error);
-            }
-        );
-    });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log("SW registered: ", registration);
+      },
+      (error) => {
+        console.error("SW registration failed: ", error);
+      },
+    );
+  });
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
